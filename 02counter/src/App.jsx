3cee448fa1ef05f19abab4c6eaad01e3bd704800
DeekshaHS2004 <deekshahs2004@gmail.com>
2025-08-24 12:47:24 +0530

    02counter updated

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -8,10 +8,13 @@ let [counter, setCounter] = useState(0)
   //let counter=15;
   const addValue=()=>{
     console.log("Clicked",counter);
-    if(counter!=20){
+    /*if(counter!=20){
       counter=counter+1;
-    }
-    setCounter(counter);
+    }*/
+    //setCounter(counter); //adds only 1 at a time
+    setCounter((prevCounter)=>prevCounter+1)
+    setCounter((prevCounter)=>prevCounter+1)
+    //Updates 2 at a time as we have 2 here bundler function works
   }
   const subtractValue=()=>{
     console.log("Clicked",counter);
